2c15769eb41ed84a31f9054f0e929cc3a5858f8e
xXCoke3Xx ~UNSC~ -ADMIN <cokeaguilar29@gmail.com>
2018-05-28 15:52:57 +0200

    Creacion de carpetas y ficheros
    Crea todas las carpetas y los ficheros necesarios

--- APLICACION/PracticaEntornos/PracticaEntornos/Form1.Designer.cs
+++ APLICACION/PracticaEntornos/PracticaEntornos/Form1.Designer.cs
@@ -31,9 +31,9 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.item1Archivo = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemSalir = new System.Windows.Forms.ToolStripMenuItem();
             this.item2Editar = new System.Windows.Forms.ToolStripMenuItem();
             this.item3Ver = new System.Windows.Forms.ToolStripMenuItem();
-            this.itemSalir = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,7 +46,8 @@
             this.item3Ver});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1148, 28);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(861, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -55,37 +56,39 @@
             this.item1Archivo.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.itemSalir});
             this.item1Archivo.Name = "item1Archivo";
-            this.item1Archivo.Size = new System.Drawing.Size(71, 24);
+            this.item1Archivo.Size = new System.Drawing.Size(60, 20);
             this.item1Archivo.Text = "Archivo";
+            // 
+            // itemSalir
+            // 
+            this.itemSalir.Name = "itemSalir";
+            this.itemSalir.Size = new System.Drawing.Size(96, 22);
+            this.itemSalir.Text = "Salir";
             // 
             // item2Editar
             // 
             this.item2Editar.Name = "item2Editar";
-            this.item2Editar.Size = new System.Drawing.Size(60, 24);
+            this.item2Editar.Size = new System.Drawing.Size(49, 20);
             this.item2Editar.Text = "Editar";
             // 
             // item3Ver
             // 
             this.item3Ver.Name = "item3Ver";
-            this.item3Ver.Size = new System.Drawing.Size(42, 24);
+            this.item3Ver.Size = new System.Drawing.Size(36, 20);
             this.item3Ver.Text = "Ver";
-            // 
-            // itemSalir
-            // 
-            this.itemSalir.Name = "itemSalir";
-            this.itemSalir.Size = new System.Drawing.Size(181, 26);
-            this.itemSalir.Text = "Salir";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1148, 763);
+            this.ClientSize = new System.Drawing.Size(861, 609);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Gestion Universidad";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
--- APLICACION/PracticaEntornos/PracticaEntornos/Form1.cs
+++ APLICACION/PracticaEntornos/PracticaEntornos/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,118 @@ namespace PracticaEntornos
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("C:/PracticaEntornosJorges/facultades.txt"))
+            {
+                MessageBox.Show("Fichero ya existe");
+            }
+            else
+            {
+                /** Creacion del fichero que contendra todas las facultades */
+                FileStream fs = File.Create("C:/PracticaEntornosJorges/facultades.txt");
+                fs.Close();
+                StreamWriter sw;
+                sw = File.CreateText("C:/PracticaEntornosJorges/facultades.txt");
+                sw.WriteLine("Medicina");
+                sw.WriteLine("Biologia");
+                sw.WriteLine("Farmacia");
+                sw.WriteLine("Veterinaria");
+                sw.WriteLine("Matemáticas");
+                sw.WriteLine("Físicas");
+                sw.WriteLine("Químicas");
+                sw.WriteLine("Telecomunicaciones");
+                sw.WriteLine("Informatica");
+                sw.WriteLine("Caminos");
+                sw.WriteLine("Naval");
+                sw.Close();
+
+                /** Creamos los directorios de cada facultad*/
+                cDirectorios();
+
+                for (int i = 0; i < 11; i++)
+                {
+                    if (i == 0)
+                    {
+                        cSubficheros("Medicina");
+                    }
+                    else if (i == 1)
+                    {
+                        cSubficheros("Biologia");
+                    }
+                    else if (i == 2)
+                    {
+                        cSubficheros("Farmacia");
+                    }
+                    else if (i == 3)
+                    {
+                        cSubficheros("Veterinaria");
+                    }
+                    else if (i == 4)
+                    {
+                        cSubficheros("Matematicas");
+                    }
+                    else if (i == 5)
+                    {
+                        cSubficheros("Fisicas");
+                    }
+                    else if (i == 6)
+                    {
+                        cSubficheros("Quimicas");
+                    }
+                    else if (i == 7)
+                    {
+                        cSubficheros("Telecomunicaciones");
+                    }
+                    else if (i == 8)
+                    {
+                        cSubficheros("Informatica");
+                    }
+                    else if (i == 9)
+                    {
+                        cSubficheros("Caminos");
+                    }
+                    else if (i == 10)
+                    {
+                        cSubficheros("Naval");
+                    }
+                }
+            }
+        }
+
+        public void cDirectorios()
+        {
+            Directory.CreateDirectory("C:/PracticaEntornosJorges/Medicina");
+            Directory.CreateDirectory("C:/PracticaEntornosJorges/Biologia");
+            Directory.CreateDirectory("C:/PracticaEntornosJorges/Farmacia");
+            Directory.CreateDirectory("C:/PracticaEntornosJorges/Veterinaria");
+            Directory.CreateDirectory("C:/PracticaEntornosJorges/Matematicas");
+            Directory.CreateDirectory("C:/PracticaEntornosJorges/Fisicas");
+            Directory.CreateDirectory("C:/PracticaEntornosJorges/Quimicas");
+            Directory.CreateDirectory("C:/PracticaEntornosJorges/Telecomunicaciones");
+            Directory.CreateDirectory("C:/PracticaEntornosJorges/Informatica");
+            Directory.CreateDirectory("C:/PracticaEntornosJorges/Caminos");
+            Directory.CreateDirectory("C:/PracticaEntornosJorges/Naval");
+        }
+
+        public void cSubficheros(String _fac)
+        {
+            FileStream fs = File.Create("C:/PracticaEntornosJorges/" + _fac + "/asignaturas.txt");
+            fs.Close();
+            FileStream fs1 = File.Create("C:/PracticaEntornosJorges/" + _fac + "/profesores.txt");
+            fs1.Close();
+            FileStream fs2 = File.Create("C:/PracticaEntornosJorges/" + _fac + "/alumnos.txt");
+            fs2.Close();
+            FileStream fs3 = File.Create("C:/PracticaEntornosJorges/" + _fac + "/solicitudMatricula.txt");
+            fs3.Close();
+            FileStream fs4 = File.Create("C:/PracticaEntornosJorges/" + _fac + "/notas.txt");
+            fs4.Close();
+            FileStream fs5 = File.Create("C:/PracticaEntornosJorges/" + _fac + "/cursos.txt");
+            fs5.Close();
+            FileStream fs6 = File.Create("C:/PracticaEntornosJorges/" + _fac + "/nominas.txt");
+            fs6.Close();
         }
     }
 }
