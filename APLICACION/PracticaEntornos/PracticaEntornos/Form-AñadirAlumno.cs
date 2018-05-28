0ebd37c8773f196a302861b2d6ab7c758dd8d885
xXCoke3Xx ~UNSC~ -ADMIN <cokeaguilar29@gmail.com>
2018-05-28 16:30:53 +0200

    Hola pepe

---
+++ APLICACION/PracticaEntornos/PracticaEntornos/Form-AñadirAlumno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticaEntornos
+{
+    public partial class Form_AñadirAlumno : Form
+    {
+        public Form_AñadirAlumno()
+        {
+            InitializeComponent();
+        }
+    }
+}
